f58c1de8f844b0c82e0aa369c3f7c43057342d5d
Harsh V Patel <harsh.v.patel@roimaint.com>
2026-01-21 01:10:21 +0530

    Add category field to products and implement filtering functionality in Home component

--- e-commerce/src/pages/Home.tsx
+++ e-commerce/src/pages/Home.tsx
@@ -7,6 +7,8 @@ const Home = () => {
 
   const [productsList, setProductsList] = useState<productInt[]>(products)
   const [searchTerm, setSearchTerm] = useState("");
+  const [filter, setFilter] = useState("")
+  const [addSkillDialogOpen, setAddSkillDialog] = useState(false);
 
   useEffect(() => {
     if (!searchTerm.trim()) {
@@ -22,6 +24,14 @@ const Home = () => {
     );
   }, [searchTerm])
 
+  useEffect(() => {
+    if (!filter.trim()) {
+      setProductsList(products);
+      return;
+    }
+    setProductsList(products.filter((p) => p.category === filter));
+  }, [filter]);
+
   return (
     <div className="p-4 flex flex-col justify-center">
       <input
@@ -31,6 +41,22 @@ const Home = () => {
         className="p-2 rounded-2xl border"
         onChange={(e) => setSearchTerm(e.target.value)}
       />
+
+      <label >
+        Filter:
+        <select
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+          className="mt-2 rounded-lg bg-white/80"
+        >
+          <option value="" >All</option>
+          <option value="men's clothing">Men's clothing</option>
+          <option value="electronics">Electronics</option>
+          <option value="women's clothing">Women's clothing</option>
+          <option value="jwellery">Jwellery</option>
+        </select>
+      </label>
+
       <div className="grid sm:grid-cols-3 gap-4 p-5">
         {productsList.map((product) => {
           return (
